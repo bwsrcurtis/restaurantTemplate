c66da1e3659023c1f01bbc43dce1540b29e9ea05
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-11 20:33:43 -0400

    File cleanup

--- src/app/components/DividerImageTwo.js
+++ src/app/components/DividerImageTwo.js
@@ -6,8 +6,8 @@ const DividerImageTwo = () => {
     <>
       <div className="w-screen h-[400px] relative">
         <div className='-z-10 absolute'>
-          <Image src='/angledCupcakes.jpg' alt="Row of Cupcakes" width={1920} height={1080}
-            className='object-cover object-right-top h-[400px]'
+          <Image src='/divMacaroons.jpg' alt="Row of Cupcakes" width={1920} height={1080}
+            className='object-cover h-[400px]'
           ></Image>
         </div>
 
