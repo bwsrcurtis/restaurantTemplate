8d799ea87d3cdefb1b59767e79e203ff07e15fe2
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-23 22:21:02 -0400

    Formattin

--- src/app/components/CardCont.js
+++ src/app/components/CardCont.js
@@ -4,9 +4,8 @@ import Card from './Card.js';
 
 const CardCont = async () => {
   const content = getAllCards('Cards');
-
-
   let cards = Object.values(content);
+
   return (
     <div id='work' className='flex flex-row flex-wrap gap-4 mx-auto justify-center pt-24'>
       {cards.map((i, key) => {
